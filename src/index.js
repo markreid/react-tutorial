abc459c889aa9d41ad0c9ee16f10505556832cab
Mark Reid <contact@markreid.com.au>
2018-03-15 22:48:59 +1100

    tutorial step 10

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ class Game extends React.Component {
         'Go to game start';
 
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
